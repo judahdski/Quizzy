f6215078a6f9bab7132771dbb2abeb7edc1e0a04
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-12 22:22:31 +0700

    feat: implementasi style
    pindahin atribut ke style lalu digunakan di masing-masing widget

--- app/src/main/java/com/d3if0002/myintentapp/ResultActivity.kt
+++ app/src/main/java/com/d3if0002/myintentapp/ResultActivity.kt
@@ -1,6 +1,7 @@
 package com.d3if0002.myintentapp
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,14 @@ import com.d3if0002.myintentapp.question.QuestionTwoActivity
 
 class ResultActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var yourAns1: TextView
+    private lateinit var yourAns2: TextView
+    private lateinit var yourAns3: TextView
+
+    private lateinit var rightAns1: TextView
+    private lateinit var rightAns2: TextView
+    private lateinit var rightAns3: TextView
+
     private lateinit var ans1: String
     private lateinit var ans2: String
     private lateinit var ans3: String
@@ -20,15 +29,9 @@ class ResultActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        supportActionBar?.title = ""
+        setUI()
 
-        ans1 = intent.getStringExtra(QuestionOneActivity.ANSWER_ONE) ?: "null"
-        ans2 = intent.getStringExtra(QuestionTwoActivity.ANSWER_TWO) ?: "null"
-        ans3 = intent.getStringExtra(QuestionThreeActivity.ANSWER_THREE) ?: "null"
 
-        findViewById<TextView>(R.id.your_ans1).text = getString(R.string.jawaban_kamu1, ans1)
-        findViewById<TextView>(R.id.your_ans2).text = getString(R.string.jawaban_kamu2, ans2)
-        findViewById<TextView>(R.id.your_ans3).text = getString(R.string.jawaban_kamu3, ans3)
 
         checkTheAnswer()
 
@@ -36,35 +39,60 @@ class ResultActivity : AppCompatActivity(), View.OnClickListener {
         findViewById<Button>(R.id.finish_btn).setOnClickListener(this)
     }
 
+    private fun setUI() {
+        supportActionBar?.title = ""
+
+        ans1 = intent.getStringExtra(QuestionOneActivity.ANSWER_ONE) ?: "null"
+        ans2 = intent.getStringExtra(QuestionTwoActivity.ANSWER_TWO) ?: "null"
+        ans3 = intent.getStringExtra(QuestionThreeActivity.ANSWER_THREE) ?: "null"
+
+        yourAns1 = findViewById(R.id.your_ans1)
+        yourAns2 = findViewById(R.id.your_ans2)
+        yourAns3 = findViewById(R.id.your_ans3)
+
+        rightAns1 = findViewById(R.id.right_ans1)
+        rightAns2 = findViewById(R.id.right_ans2)
+        rightAns3 = findViewById(R.id.right_ans3)
+
+        yourAns1.text = getString(R.string.jawaban_kamu1, ans1)
+        yourAns2.text = getString(R.string.jawaban_kamu2, ans2)
+        yourAns3.text = getString(R.string.jawaban_kamu3, ans3)
+    }
+
     private fun checkTheAnswer() {
         var count = 0
 
-        val rightAns1 = "Big Black Cock"
-        val rightAns2 = "Niel"
-        val rightAns3 = "Sense of humors"
+        val trueColor = Color.parseColor("#00FF00")
+        val falseColor = Color.parseColor("#B00020")
+        val rightAnswer1 = "Big Black Cock"
+        val rightAnswer2 = "Niel"
+        val rightAnswer3 = "Sense of humors"
 
-        if (ans1 == rightAns1) {
+        if (ans1 == rightAnswer1) {
             count++
+            yourAns1.setTextColor(trueColor)
         } else {
-            val rightAns = findViewById<TextView>(R.id.right_ans1)
-            rightAns.text = getString(R.string.jawaban_benar1, rightAns1)
-            rightAns.visibility = View.VISIBLE
+            yourAns1.setTextColor(falseColor)
+            rightAns1.text = getString(R.string.jawaban_benar1, rightAnswer1)
+            rightAns1.visibility = View.VISIBLE
         }
 
-        if (ans2 == rightAns2) {
+        if (ans2 == rightAnswer2) {
             count++
+            yourAns2.setTextColor(trueColor)
         } else {
-            val rightAns = findViewById<TextView>(R.id.right_ans2)
-            rightAns.text = getString(R.string.jawaban_benar1, rightAns2)
-            rightAns.visibility = View.VISIBLE
+            yourAns2.setTextColor(falseColor)
+            rightAns2.text = getString(R.string.jawaban_benar1, rightAnswer2)
+            rightAns2.visibility = View.VISIBLE
         }
 
-        if (ans3 == rightAns3) {
+        if (ans3 == rightAnswer3) {
             count++
+            yourAns3.setTextColor(trueColor)
         } else {
-            val rightAns = findViewById<TextView>(R.id.right_ans3)
-            rightAns.text = getString(R.string.jawaban_benar1, rightAns3)
-            rightAns.visibility = View.VISIBLE
+            yourAns3.setTextColor(falseColor)
+            rightAns3.text = getString(R.string.jawaban_benar1, rightAnswer3)
+            rightAns3.visibility = View.VISIBLE
         }
 
         findViewById<TextView>(R.id.score_indicator).text = getString(R.string.score, count.toString())
